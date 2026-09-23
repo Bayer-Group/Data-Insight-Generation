5d77a9c0f99942a58c6ea9eefc7592c0038b8262
VSchmidtBayer <veronika.schmidt.ext@bayer.com>
2026-09-23 12:22:27 +0000

    adapt for other use

--- R/start_app.R
+++
@@ -1,8 +0,0 @@
-#' start webapp
-#'
-#' @export
-#' @rdname startApplication
-start_app <- function() {
-  appDir <- system.file("/app", package = "safetygraphicsif")
-  shiny::runApp(appDir, launch.browser = TRUE)
-}
